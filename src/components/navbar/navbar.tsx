5c3c31ff5ac6d5be0a9802d7cc41c1d6f27f1b2a
Seu Nome <lucasfinatticode@gmail.com>
2025-06-19 10:28:57 -0300

    [FIX] Files to build

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
 import './navbar.scss';
 
 export default function Navbar() {
     return (
         <div className="navbar">
-            {/* <div>
-                <img className='navbar__logo' src="/photo-camera2.jpg" width={50} />
-            </div> */}
             <div className='navbar__title'>Emoldure sua lembrança 😎👍</div>
         </div>
     );
